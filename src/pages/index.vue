8d3ee6994af5b1c9d6e0c316cd5f04621288db32
BrownMing <168336364+BrownMing@users.noreply.github.com>
2024-05-29 16:54:01 +0800

    zuihou

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 p-2 p-x-5 text-8 color-white class="email_text">
-        639293382@qq.com
+        hedgebethbouthot@gmail.com
       </div>
     </div>
   </div>
