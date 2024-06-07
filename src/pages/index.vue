9b7891a6e36d4f683d9b82d9b117547eb61a0cb5
BrownMing <168336364+BrownMing@users.noreply.github.com>
2024-06-07 17:54:33 +0800

    2222

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,16 +9,16 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 p-2 p-x-5 text-8 color-white>
+      <div rd-2 p-2 p-x-5 text-8 color-white class="email_text">
         nooneypline@gmail.com </div>
     </div>
   </div>
 </template>
 <style scoped lang="css">
-.email {
-  opacity: 1;
-  border-radius: 16px;
-  background: radial-gradient(6.57% 50% at 50.14925373134328% 100%, #F5F5F5 0%, #F5F5F5 100%), linear-gradient(135deg, #FF5833 0%, #FF7C33 100%);
-  backdrop-filter: blur(40px);
+.email_text {
+background: radial-gradient(6.57% 50% at 50.14925373134328% 100%, #F5F5F5 0%, #F5F5F5 100%), linear-gradient(135deg, #FF5833 0%, #FF7C33 100%);
+box-shadow: 0px 8px 16px  #FF6712;
+backdrop-filter: blur(40px);
+
 }
 </style>
