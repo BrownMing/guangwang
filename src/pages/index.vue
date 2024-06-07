ddb943fbc5d47bbb97f7f51a5af537ff00e22258
BrownMing <168336364+BrownMing@users.noreply.github.com>
2024-06-07 17:42:52 +0800

    1111

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,16 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
-      </div>
+      <div rd-2 p-2 p-x-5 text-8 color-white>
+        nooneypline@gmail.com </div>
     </div>
   </div>
 </template>
+<style scoped lang="css">
+.email {
+  opacity: 1;
+  border-radius: 16px;
+  background: radial-gradient(6.57% 50% at 50.14925373134328% 100%, #F5F5F5 0%, #F5F5F5 100%), linear-gradient(135deg, #FF5833 0%, #FF7C33 100%);
+  backdrop-filter: blur(40px);
+}
+</style>
