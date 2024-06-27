23f0ee4144f87ea776e9583f6dd15079fc83bf34
BrownMing <168336364+BrownMing@users.noreply.github.com>
2024-06-27 19:13:25 +0800

    155445

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,14 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
+      <div rd-2 p-2 p-x-5 text-8 color-white class="email_box">
         639293382@qq.com
       </div>
     </div>
   </div>
 </template>
+<style lang="css" scoped>
+.email .email_box {
+  background: linear-gradient(119.46deg, rgba(189, 209, 255, 1) 0%, rgba(239, 166, 255, 1) 58.34%, rgba(253, 222, 255, 1) 100%);
+}
+</style>
