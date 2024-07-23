29e4f9fe2b546bf2a03c1f858cf92afa713452b6
BrownMing <168336364+BrownMing@users.noreply.github.com>
2024-07-23 15:30:03 +0800

    4789453456

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,14 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        639293382@qq.com
+      <div rd-2 bg-black p-2 p-x-5 text-8 color-white class="email_box">
+        HaithemSalgado9@gmail.com
       </div>
     </div>
   </div>
 </template>
+<style lang="css" scoped>
+.email_box {
+  margin-bottom: 20px;
+}
+</style>
