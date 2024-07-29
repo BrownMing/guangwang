260676d15fb47f0083a1674e9c1567c36fe80e1e
BrownMing <168336364+BrownMing@users.noreply.github.com>
2024-07-29 10:58:31 +0800

    scss

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,9 +9,18 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
+      <div rd-2 bg-black p-2 p-x-5 text-8 color-white class="email_sbox">
         639293382@qq.com
       </div>
     </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.email {
+  .email_sbox {
+    position: absolute;
+    top: -83px;
+    right: 370px;
+  }
+}
+</style>
