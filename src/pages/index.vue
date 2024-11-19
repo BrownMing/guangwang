3c984ad54c12501a08a40175b71dd1e65b70806b
BrownMing <168336364+BrownMing@users.noreply.github.com>
2024-11-19 14:38:31 +0800

    324232

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-white p-2 p-x-5 text-8 color-black>
-        639293382@qq.com
+        lixiaoyun1007@icloud.com
       </div>
     </div>
   </div>
--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -2,7 +2,7 @@
 defineOptions({
   name: 'IndexPage',
 })
-const appName = 'Kive'
+const appName = 'Rilon'
 </script>
 
 <template>
